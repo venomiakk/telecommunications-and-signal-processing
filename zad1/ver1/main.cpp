63c752e308273050caf7e57a2644b6f912c8d7f7
Adrian <147884387+venomiakk@users.noreply.github.com>
2024-03-18 22:48:39 +0100

    1bit correction

--- zad1/ver1/main.cpp
+++ zad1/ver1/main.cpp
@@ -39,14 +39,9 @@ void printMatrix(int matrix[8][16]) {
     }
 }
 
-void changeBits(int &bit) {
-    if (bit == 1){
-        bit = 0;
-    } else {
-        bit = 1;
-    }
-}
-
+/*
+ * Funkcja zamieniajaca wprowadzony tekst na tablice vector intów
+ */
 void txtToInt(vector<int> &msg, string txt) {
     for (int i = 0; i < txt.size(); ++i) {
         msg.push_back((int) txt.at(i) - 48);
@@ -54,61 +49,98 @@ void txtToInt(vector<int> &msg, string txt) {
 }
 
 /*
- *  Przy kodowaniu:
- *      Obliczanie bitów parzystości
- *      Każdy bit parzystości jest obliczany jako iloczyn bitów wiadomości i bitów danego wiersza
+ * Funkcja odpowiedzialna za mnożenie wektorów
  */
 int multiplyVectors(vector<int> &T, int row) {
     int output = 0;
-    for (int i = 0; i < row; ++i) {
+    for (int i = 0; i < 8; ++i) {
         output += sbH[row][i] * T[i];
-        cout<<output<<", ";
     }
     return output % 2;
 }
 
-//Kodowanie podanej wiadomości poprzez dodanie bitów parzystości
-void encode(vector<int> &msg) {
+/*
+ * Kodowanie podanej wiadomości poprzez dodanie bitów parzystości
+ * Jest to rozwiązane poprzez mnożenie wektorów
+ * Każdy wiersz macierzy H jest mnożony przez wektor wiadomości T
+ * Następnie z sumy elementów wektora wyjściowego wyciągana jest wartość 0 lub 1 poprzez operację modulo 2
+*/
+ void encode(vector<int> &msg) {
     for (int i = 0; i < sbH_ROWS; ++i) {
         msg.push_back(multiplyVectors(msg, i));
     }
 }
 
 /*
- * Dekodowanie wiadomości
+ * Funkcja odpowiadająca za poprawianie wiadomości
+ * Wektor błędów porównywany jest z odpowiednią kolumną macierzy H
+ * Gdy wektory są sobie równe, błąd wystąpił na miejscu odpowiadającym danej kolumnie macierzy H
+ */
+
+void correctMsg(vector<int> &E, vector<int> &T){
+    for (int i = 0; i < 8; ++i) {
+        int count = 0;
+        for (int j = 0; j < 4; ++j) {
+            if (E[j] == sbH[j][i]){
+                count++;
+            }
+        }
+        if (count == 4){
+            cout<<"Blad wystapil na miejscu "<<i+1<<endl;
+            if (T[i] == 1){
+                T[i] = 0;
+            } else {
+                T[i] = 1;
+            }
+            return;
+        }
+    }
+}
+
+/*
+ * Funkcja odpowiedzialna za sprawdzenie czy wiadomosc jest poprawna
+ * Rzędy macierzy H są mnożone z wektorem wiadomości T a do wyniku dodawany jest odpowiedni bit parzystości
+ * Gdy suma elementów wektora wyjściowego jest równa 0, wtedy błąd nie wystąpił
+ * Gdy suma nie jest równa 0, następuje poprawienie wiadomości
  */
 void checkMsg (vector<int> &T){
     vector<int> E;
     int sum = 0;
     for (int i = 0; i < sbH_ROWS; ++i) {
-        E.push_back(multiplyVectors(T, i));
-        cout<<E[i]<<endl;
+        E.push_back(multiplyVectors(T, i) + T[8+i]);
+        E[i] = E[i] % 2;
         sum += E[i];
     }
+
     if (sum!=0){
-        cout<<"Wrong msg!";
+        cout<<"Niepoprawna wiadomosc"<<endl;
+        correctMsg(E, T);
+        cout<<"Poprawiona wiadomosc: ";
+        for (int i = 0; i < T.size(); ++i) {
+            cout<<T[i];
+        }
     } else{
-        cout<<"Correct msg!";
+        cout<<"Poprawna wiadomosc";
     }
 }
 
 int main() {
 
-    cout << "Kodowanie:" <<endl;
-    vector<int> input;
-    string txt = "11000101";
-    txtToInt(input, txt);
-    encode(input);
-    for (int i = 0; i < input.size(); ++i) {
-        cout << input[i];
-    }
+//    cout << "Kodowanie:" <<endl;
+//    vector<int> input;
+//    string txt = "11000101";
+//    txtToInt(input, txt);
+//    encode(input);
+//    for (int i = 0; i < input.size(); ++i) {
+//        cout << input[i];
+//    }
 //    system("pause");
     cout<<endl;
     cout << "Dekodowanie:" <<endl;
     vector<int> invalidInput;
-    string valid = "110001010111";
-    string invalid = "11000010111";
-    txtToInt(invalidInput, valid);
+    string valid = "110001011100";
+    string invalid = "111001011100";
+    txtToInt(invalidInput, invalid);
     checkMsg(invalidInput);
     return 0;
 }
