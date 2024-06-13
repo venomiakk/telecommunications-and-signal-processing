e5a7289bc131c613dba05566295c670ba73b615e
Adrian <147884387+venomiakk@users.noreply.github.com>
2024-06-14 00:56:41 +0200

    lalala 🔊🔉🔈🙊

--- zad4/audio.py
+++ zad4/audio.py
@@ -1,49 +1,115 @@
 import pyaudio
 import wave
+import numpy as np
 
-ISRECORDING = True
+IS_RECORDING = False
+CHUNK = 1024
+FORMAT = pyaudio.paInt16
 
-def set_recording():
-    ISRECORDING = False
+LAST_SNR = -1.0
 
-def save_recording():
-    # Konfiguracja audio
-    FORMAT = pyaudio.paInt16
+
+def quantize(data, levels):
+    data = np.frombuffer(data, dtype=np.int16)
+    max_val = np.max(data)
+    min_val = np.min(data)
+    delta =  (max_val - min_val) / (levels - 1)
+    quantized = delta * np.floor((data/delta) + 0.5)
+    return quantized.astype(np.int16)
+
+
+def dequantize(quantized_data, levels):
+    # np type?
+    X_min = np.min(quantized_data)
+    X_max = np.max(quantized_data)
+    delta = (X_max - X_min) / (levels - 1)
+    dequantized = delta * (np.floor((quantized_data / delta) + 0.5) - 0.5)
+    return dequantized.astype(np.int16)
+
+
+def calculate_snr(original_signal, quantized_signal):
+    original_signal = np.frombuffer(original_signal, dtype=np.int16)
+    signal_power = np.mean(np.square(original_signal))
+    noise = original_signal - quantized_signal
+    noise_power = np.mean(np.square(noise))
+    snr = 10 * np.log10(signal_power / noise_power)
+    print(f"SNR: {snr}")
+    return snr
+
+
+def stop_recording():
+    global IS_RECORDING
+    IS_RECORDING = False
+    print("Nagrywanie zakończone.")
+
+
+def start_recording(sample_rate, quantization_lvl):
     CHANNELS = 1
-    RATE = 44100
-    CHUNK = 1024
-    RECORD_SECONDS = 10
+    RATE = sample_rate
     WAVE_OUTPUT_FILENAME = "recorded_audio.wav"
 
     audio = pyaudio.PyAudio()
 
-    # Inicjalizacja nagrywania
     stream = audio.open(format=FORMAT, channels=CHANNELS,
                         rate=RATE, input=True,
                         frames_per_buffer=CHUNK)
 
-    print("Nagrywanie...")
+    print(f"Nagrywanie, {sample_rate}Hz, {quantization_lvl} poziomow")
 
     frames = []
+    global IS_RECORDING
+    IS_RECORDING = True
 
-    for i in range(0, int(RATE / CHUNK * RECORD_SECONDS)):
+    while IS_RECORDING:
         data = stream.read(CHUNK)
         frames.append(data)
 
     print("Zakończono nagrywanie.")
 
-    # Zatrzymanie nagrywania
     stream.stop_stream()
     stream.close()
     audio.terminate()
 
-    # Zapisanie dźwięku do pliku WAV
+    audio_data = b''.join(frames)
+    quantized_audio = quantize(audio_data, quantization_lvl)
+    snr = calculate_snr(audio_data, quantized_audio)
+    global LAST_SNR
+    LAST_SNR = np.round(snr, 2)
+
     wf = wave.open(WAVE_OUTPUT_FILENAME, 'wb')
     wf.setnchannels(CHANNELS)
     wf.setsampwidth(audio.get_sample_size(FORMAT))
     wf.setframerate(RATE)
-    wf.writeframes(b''.join(frames))
+    wf.writeframes(quantized_audio.tobytes())
     wf.close()
+
+
+
+def play_audio():
+    try:
+        wf = wave.open('recorded_audio.wav', 'rb')
+    except FileNotFoundError:
+        print("Plik 'recorded_audio.wav' nie został znaleziony.")
+        return False
+
+    p = pyaudio.PyAudio()
+
+    stream = p.open(format=p.get_format_from_width(wf.getsampwidth()),
+                    channels=wf.getnchannels(),
+                    rate=wf.getframerate(),
+                    output=True)
+
+    data = wf.readframes(CHUNK)
+    while len(data) > 0:
+        stream.write(data)
+        data = wf.readframes(CHUNK)
+
+    stream.stop_stream()
+    stream.close()
+
+    p.terminate()
+
+    return True
 
 def load_recording():
     # Wczytanie pliku WAV
@@ -57,4 +123,4 @@ def load_recording():
     CHUNK = 1024
 
 if __name__ == "__main__":
-    save_recording()
+    print("audio module")
--- zad4/gui.py
+++ zad4/gui.py
@@ -1,12 +1,10 @@
 import tkinter as tk
+from tkinter import messagebox
 import re
+import threading
+import time
+import audio
 
-
-def test1():
-    print("test1")
-
-def test2(txt):
-    print(txt)
 
 def only_int(P):
     if re.match("^[0-9]*$", P):
@@ -39,13 +37,43 @@ def show_interface():
     quantization_lvl.place(x=570, y=50)
 
     sample_rate.insert(0, "44100")
-    quantization_lvl.insert(0, "16")
-
-    btn1 = tk.Button(text="Btn1", command=lambda: test1(), state="disabled")
-    btn1.place(x=100, y=200)
-    btn2 = tk.Button(text="Btn2", command=lambda: test2("asv"))
-    btn2.place(x=100, y=300)
+    quantization_lvl.insert(0, "256")
     
+
+    startRecBtn = tk.Button(text="Start", command=lambda: start_audio_recording())
+    startRecBtn.place(x=100, y=200)
+    stopRecBtn = tk.Button(text="Stop", command=lambda: stop_audio_recording())
+    stopRecBtn.place(x=100, y=250)
+
+    playRecBtn = tk.Button(text="Odtwórz", command=lambda: play_recorded_audio())
+    playRecBtn.place(x=100, y=300)
+    
+    def start_audio_recording():
+        startRecBtn.config(state="disabled")
+        sr = int(sample_rate.get())
+        ql = int(quantization_lvl.get())
+        threading.Thread(target=audio.start_recording, args=(sr, ql)).start()
+
+
+    def stop_audio_recording():
+        startRecBtn.config(state="normal")
+        audio.stop_recording()
+        
+        frame = tk.Frame(root, width=100, height=100, bg='#f0f0f0')
+        frame.place(x=140, y=190)
+        
+        # :(
+        time.sleep(0.2)
+        snr = audio.LAST_SNR
+        snrLab = tk.Label(root, text=f"SNR: {snr} dB")
+        snrLab.place(x=150, y=200)
+
+
+    def play_recorded_audio():
+        played = audio.play_audio()
+        if not played:
+            messagebox.showerror("Błąd", "Plik 'recorded_audio.wav' nie został znaleziony.")
+
     root.mainloop() 
 
 
